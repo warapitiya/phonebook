dfd3039f818a22e9764091dc36aace16acd1adc9
Malindu Warapitiya <mwarapitiya@virtusa.com>
2016-03-15 12:42:29 +0530

    change few design elements and add new functions

--- views/home/home.controller.js
+++ views/home/home.controller.js
@@ -7,9 +7,20 @@ angular.module('phonebook.controllers', [])
 
 function homeController($http) {
 
+    //scope
     var vm = this;
 
+    //list of contacts
     vm.phonebooklist = [];
+
+    //contact details object
+    vm.phonebookdetails = {};
+
+    //copy of contact details object with active property
+    vm.phonebookselected = {};
+
+    //item click event function
+    vm.itemClicked = itemClicked;
 
 
     /**
@@ -20,6 +31,29 @@ function homeController($http) {
             vm.phonebooklist = _data.data;
         }, function (_error) {
             console.error(_error);
-        })
+        });
+
+
+    /**
+     * on click event for item click
+     * @param _record
+     */
+    function itemClicked(_record) {
+
+        //skip if not null
+        if (!_.isNull(vm.phonebookselected)) {
+            //delete the active property
+            delete vm.phonebookselected.active;
+        }
+
+        //add active property
+        _record.active = true;
+
+        //cache a copy of _record
+        vm.phonebookselected = _record;
+
+        //deep clone the object
+        vm.phonebookdetails = _.clone(_record);
+    }
 
 }
